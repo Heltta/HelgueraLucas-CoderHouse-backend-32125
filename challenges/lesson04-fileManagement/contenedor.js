ebb0b42d07ac51bc5525c6df06ccf47a7198ba44
Heltta <lucashelgueraz@gmail.com>
2022-09-02 20:51:53 -0300

    Create contenedor.js
    Create contenedor.js and the skeleton of the Contenedor class.

---
+++ challenges/lesson04-fileManagement/contenedor.js
@@ -0,0 +1,31 @@
+class Contenedor {
+    constructor(){
+
+    }
+
+    save(data){
+        //Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
+        let id;
+        return id
+    }
+
+    getById(id){
+        //Recibe un id y devuelve el objeto con ese id, o null si no está.
+        let obj = null;
+        return obj
+    }
+
+    getAll(){
+        //Devuelve un array con los objetos presentes en el archivo.
+        let objs = [];
+        return objs
+    }
+
+    deleteById(id){
+        //Elimina del archivo el objeto con el id buscado
+    }
+
+    deleteAll(){
+        //Elimina todos los objetos presentes en el archivo.
+    }
+}
