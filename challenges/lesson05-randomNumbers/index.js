60d4155ca1a41d89406536814b345bc9d97c8465
Heltta <lucashelgueraz@gmail.com>
2022-09-06 11:03:28 -0300

    Create obj with random numbers frequency as keys

--- challenges/lesson05-randomNumbers/index.js
+++ challenges/lesson05-randomNumbers/index.js
@@ -5,3 +5,14 @@ for(let i =0 ; i<=10000; i++){
     rNums.push(Math.ceil(Math.random()*20))
 }
 
+const keys = {};
+
+for(let i =1; i<=20; i++){
+    keys[i]=0;
+}
+
+rNums.forEach((num)=>{
+    keys[num]++;
+})
+
+console.log(keys);
