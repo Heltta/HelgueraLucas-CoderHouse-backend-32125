cd5636c190993504599094aa168cd979a6ec133f
Heltta <lucashelgueraz@gmail.com>
2022-09-08 22:58:35 -0300

    Overhaul index.js testing script

--- challenges/lesson04-fileManagement/index.js
+++ challenges/lesson04-fileManagement/index.js
@@ -2,22 +2,49 @@ import Contenedor from "./contenedor.js";
 
 const contenedor = new Contenedor('challenges/lesson04-fileManagement/test/content.json');
 
-contenedor.save(
-    {
-        title: 'Champion',
-        price: 123.45,
-        thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
-    }
-);
+const testingObj = {
+    title: 'Champion',
+    price: 123.45,
+    thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',
+}
 
+
+//Test save
 try{
-    const objFound = await contenedor.getById(2);
+    console.log('Test save');
+    await contenedor.save(testingObj);
+    await contenedor.save(testingObj);
+    console.log('Objects added');
 }
 catch(error){
     console.log(error)
 }
 
+//Test getById
 try{
+    console.log('Test getById');
+    const objFound = await contenedor.getById(2);
+    console.log(objFound);
+}
+catch(error){
+    console.log(error)
+}
+
+//Test getAll
+try{
+    console.log('Test getAll');
+    const allObjs = await contenedor.getAll()
+    console.log( allObjs );
+}
+catch(error){
+    console.log(error)
+}
+
+//Test deleteById
+try{
+    //Test deleteById when id doesn't exist
+    console.log('Test deleteById when id doesn\'t exist (id = 798764)');
+    await contenedor.deleteById(798764);
     const allObjs = await contenedor.getAll()
     console.log( allObjs );
 }
@@ -26,6 +53,19 @@ catch(error){
 }
 
 try{
+    //Test deleteById when id exists
+    console.log('Test deleteById when id exists (id = 2)');
+    await contenedor.deleteById(2);
+    const allObjs = await contenedor.getAll()
+    console.log( allObjs );
+}
+catch(error){
+    console.log(error)
+}
+
+//Test deleteAll
+try{
+    console.log('Test deleteAll');
     await contenedor.deleteAll()
     const allObjs = await contenedor.getAll()
     console.log( allObjs );
