0b190a716989bc391ce3e20f30c5f460ec89dbf4
Heltta <lucashelgueraz@gmail.com>
2022-09-15 19:01:37 -0300

    Configure optional PORT

--- challenges/lesson06-expressServer/server.js
+++ challenges/lesson06-expressServer/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const Contenedor = require('./modules/contenedor');
 const app = express();
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 const server = app.listen(PORT, () => {
     console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
 });
