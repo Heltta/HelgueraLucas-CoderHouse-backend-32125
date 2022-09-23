81f75ee4255001ec9f9c28b0a12658c13a4edb4a
Heltta <lucashelgueraz@gmail.com>
2022-09-22 22:30:14 -0300

    GetAll and GetById implemented

--- challenges/lesson08-apiRestful/server.js
+++ challenges/lesson08-apiRestful/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const multer = require('multer');
+const Contenedor = require('./modules/productos');
 
 const app = express();
 const { json } = express;
@@ -7,7 +7,7 @@ const { json } = express;
 app.use(json());
 
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 const PORT = process.env.PORT || 8080
@@ -15,36 +15,51 @@ const server = app.listen(PORT, () => {
     console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
 });
 
-let storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads')
-    },
-    filename: (req, file, cb) => {
-        console.log(file)
-        cb(null, file.fieldname + '-' + Date.now())
-    }
+//Multer
+// const multer = require('multer');
+
+// let storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'uploads')
+//     },
+//     filename: (req, file, cb) => {
+//         console.log(file)
+//         cb(null, file.fieldname + '-' + Date.now())
+//     }
+// })
+
+// const upload = multer({ storage: storage })
+
+// app.post('/uploadfile', upload.single('myfile') ,(req, res, next) => {
+//     const file = req.file
+//     if (!file) {
+//         const error = new Error('Please upload a file')
+//         error.httpStatusCode = 400
+//         return next(error)
+//     }
+//     res.send(file)
+// })
+
+// app.post('/uploadmultiple', upload.array('myfiles',12) ,(req, res, next) => {
+//     const files = req.files
+//     if (!files) {
+//         const error = new Error('Please upload a file')
+//         error.httpStatusCode = 400
+//         return next(error)
+//     }
+//     res.send(files)
+// })
+
+
+const products = new Contenedor('./uploads/productos.json');
+
+
+app.get('/api/productos', (req, res) => {
+    products.getAll()
+        .then((products)=>res.send(products));
 })
 
-const upload = multer({ storage: storage })
-
-app.post('/uploadfile', upload.single('myfile') ,(req, res, next) => {
-    const file = req.file
-    if (!file) {
-        const error = new Error('Please upload a file')
-        error.httpStatusCode = 400
-        return next(error)
-    }
-    res.send(file)
+app.get('/api/productos/:id', (req, res) => {
+    products.getById(parseInt(req.params.id))
+        .then((product)=>res.send(product));
 })
-
-app.post('/uploadmultiple', upload.array('myfiles',12) ,(req, res, next) => {
-    const files = req.files
-    if (!files) {
-        const error = new Error('Please upload a file')
-        error.httpStatusCode = 400
-        return next(error)
-    }
-    res.send(files)
-})
-
-
