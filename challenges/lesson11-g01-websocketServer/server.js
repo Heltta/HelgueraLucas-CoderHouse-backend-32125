ea7273af6afd6d00108d830b583c3e9299dbb7d5
Heltta <lucashelgueraz@gmail.com>
2022-09-27 11:49:13 -0300

    Initialize server.js server required libraries

---
+++ challenges/lesson11-g01-websocketServer/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+
+const app = express()
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
