357de9c3dec4f7d1953f721dce4ac2294e6d4d12
Heltta <lucashelgueraz@gmail.com>
2022-09-29 15:34:28 -0300

    Initialize server

--- challenges/lesson11-g01-websocketServer/server.js
+++ challenges/lesson11-g01-websocketServer/server.js
@@ -5,3 +5,16 @@ const { Server: IOServer } = require('socket.io')
 const app = express()
 const httpServer = new HttpServer(app)
 const io = new IOServer(httpServer)
+
+// Indicamos que queremos cargar los archivos estáticos que se encuentran en dich a carpeta
+app.use(express.static('./public'))
+// Esta ruta carga nuestro archivo index.html en la raíz de la misma
+app.get('/', (req, res) => {
+res.sendFile('index.html', { root: __dirname })
+})
+// El servidor funcionando en el puerto 3000
+const PORT = process.env.PORT || 3000
+const server = httpServer.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+});
+server.on("error", error => console.log(`Error en servidor ${error}`));
