84b6a5477d3d18fd238b2f7262c67ee5b62a0e97
Heltta <lucashelgueraz@gmail.com>
2022-09-29 19:32:46 -0300

    Init challenge folder

---
+++ challenges/lesson09-g02-customTemplateEngine/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+//same as app.use(express.json())
+const { json } = express;
+app.use(json());
+
+// app.use(express.urlencoded({ extended: true}));
+
+app.use(express.static("./public"))
+
+const PORT = process.env.PORT || 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+});
+server.on("error", error => console.log(`Error en servidor ${error}`));
+
