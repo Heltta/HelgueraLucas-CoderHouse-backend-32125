d2937f6c61e81305d8533321b7a22a8c0e8eeac9
Heltta <lucashelgueraz@gmail.com>
2022-09-29 20:32:30 -0300

    Render first custom template

--- challenges/lesson09-g02-customTemplateEngine/server.js
+++ challenges/lesson09-g02-customTemplateEngine/server.js
@@ -17,7 +17,8 @@ const server = app.listen(PORT, () => {
 server.on("error", error => console.log(`Error en servidor ${error}`));
 
 const fs = require('fs');
-// defino el motor de plantilla
+
+// defino el motor de plantilla de ejemplo
 app.engine('ntl', function (filePath, options, callback) {
     fs.readFile(filePath, function (err, content) {
         if (err) {
@@ -34,6 +35,37 @@ app.engine('ntl', function (filePath, options, callback) {
 app.set('views', './views'); // especifica el directorio de vistas
 app.set('view engine', 'ntl'); // registra el motor de plantillas
 
+// renderizo una vista usando el motor custom del ejemplo
 app.get('/', function (req, res) {
     res.render('index', { title: 'Hey', message: 'Hello there!'});
+});
+
+// defino motor de plantilla del desafío
+app.engine('cte', function (filePath, options, callback) {
+    fs.readFile(filePath, function (err, content) {
+        if (err) {
+            return callback(new Error(err));
+        }
+
+        const rendered = content.toString()
+            .replace('^^titulo$$', ''+ options.titulo +'')
+            .replace('^^mensaje$$', ''+ options.mensaje +'')
+            .replace('^^autor$$', ''+ options.autor +'')
+            .replace('^^version$$', ''+ options.version +'');
+        
+        return callback(null, rendered);
+    });
+});
+app.set('views', './views'); // especifica el directorio de vistas
+app.set('view engine', 'cte'); // registra el motor de plantillas
+
+// renderizo una vista usando el motor custom del desafio
+app.get('/cte1', function (req, res) {
+    res.render('plantilla1', {
+        titulo:     "Plantilla custom para el desafío",
+        mensaje:    "Esta es la ruta a la plantilla 1",
+        autor:      "Pepe botella",
+        version:    "1.0.0"
+        }
+        );
 });
