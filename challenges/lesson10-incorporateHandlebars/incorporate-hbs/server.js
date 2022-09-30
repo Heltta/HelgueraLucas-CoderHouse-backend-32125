8f78be8632091c8463ae6add5f35dbb25bb42f53
Heltta <lucashelgueraz@gmail.com>
2022-09-30 02:32:24 -0300

    Use HandleBars

--- challenges/lesson10-incorporateHandlebars/incorporate-hbs/server.js
+++ challenges/lesson10-incorporateHandlebars/incorporate-hbs/server.js
@@ -7,11 +7,45 @@ const app = express();
 const { json } = express;
 app.use(json());
 
+// load handlebars
+const handlebars = require('express-handlebars');
+
+// config handlebars
+app.engine(
+    "hbs", // template engine reference name (it will be used later with a .set())
+    handlebars.engine({ // handlebars config function
+        extname: ".hbs", // extension used (instead of default .handlebars)
+        defaultLayout: 'index.hbs', // main template
+        layoutsDir: __dirname+'/views/layouts', // main template directory route
+        partialsDir: __dirname+'/views/partials', // partials templates directory route
+    })
+);
+
+app.set('view engine', 'hbs'); // register template engine
+app.set('views', './views'); // set template files folder
+app.use(express.static("./public")) // set server public space
+
 app.use(express.urlencoded({ extended: true}));
 
 app.use('/api/productos', products);
 app.use(express.static("./public"))
 
+const userData = { 
+    titulo: 'CoderHouse',
+    nombre: 'Pepe',
+    apellido: 'Botella',
+    edad: '40',
+    email: 'pepito_botellon@hotmail.com',
+    telefono: '+599 91 123 123'
+};
+
+
+// renderizo una vista de home
+app.get('/',  (req, res) => {
+    res.render('main', userData);
+});
+
+// Start server
 const PORT = process.env.PORT || 8080
 const server = app.listen(PORT, () => {
     console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
