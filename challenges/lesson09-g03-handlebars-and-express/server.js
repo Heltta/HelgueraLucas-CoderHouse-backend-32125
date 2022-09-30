0cb04367730970f0713dc3f9f438d9e2294e785a
Heltta <lucashelgueraz@gmail.com>
2022-09-29 22:17:56 -0300

    First render accomplished

--- challenges/lesson09-g03-handlebars-and-express/server.js
+++ challenges/lesson09-g03-handlebars-and-express/server.js
@@ -11,14 +11,29 @@ app.engine(
     handlebars.engine({ // handlebars config function
         extname: ".hbs", // extension used (instead of default .handlebars)
         defaultLayout: 'index.hbs', // main template
-        layoutsDir: __dirname + '/views/layouts', // main template directory route
-        partialsDir: __dirname + 'views/partials', // partials templates directory route
+        layoutsDir: __dirname+'/views/layouts', // main template directory route
+        partialsDir: __dirname+'/views/partials', // partials templates directory route
     })
 );
 
-app.set('view engine', 'cte'); // register template engine
+app.set('view engine', 'hbs'); // register template engine
 app.set('views', './views'); // set template files folder
 app.use(express.static("./public")) // set server public space
+
+const userData = { 
+    titulo: 'CoderHouse',
+    nombre: 'Pepe',
+    apellido: 'Botella',
+    edad: '40',
+    email: 'pepito_botellon@hotmail.com',
+    telefono: '+599 91 123 123'
+};
+
+
+// renderizo una vista de home
+app.get('/',  (req, res) => {
+    res.render('main', {});
+});
 
 
 const PORT = process.env.PORT || 8080
