df53f05f6d3a91bd85e3cdb156b6f37a148703ab
Heltta <lucashelgueraz@gmail.com>
2022-10-04 13:45:44 -0300

    Copy and paste code from G01

--- challenges/lesson13-g02-randomColorWithTSC/server.ts
+++ challenges/lesson13-g02-randomColorWithTSC/server.ts
@@ -2,3 +2,31 @@ const lista:Array<number>=[2,3,5,7];
 
 lista.map((x:number) => x*x)
     .forEach((x:number) => console.log(x));
+
+    
+// Generic challenge
+class Paleta{
+    constructor(mode="RGB"){
+        if(mode === "RGB"){
+            this.colors = [];
+        }
+    }
+
+    randomRGBColor(){
+        return Math.floor(Math.random() * 256)
+    }
+
+    addRandomRGBtoPallet(){
+        this.colors.push(
+            {
+                red: this.randomRGBColor(),
+                green:this.randomRGBColor(),
+                blue: this.randomRGBColor(),
+            }
+        )
+    }
+}
+
+const paleta = new Paleta();
+paleta.addRandomRGBtoPallet();
+console.log(paleta.colors);
