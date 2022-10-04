b28f83b1d7b4e78c5bdcb7d499a7f0cd8d7daa85
Heltta <lucashelgueraz@gmail.com>
2022-10-04 14:21:56 -0300

    Update build and test
    Generic challenge number 2 of lesson 13 finished.

--- challenges/lesson13-g02-randomColorWithTSC/server.js
+++ challenges/lesson13-g02-randomColorWithTSC/server.js
@@ -1,3 +1,26 @@
 var lista = [2, 3, 5, 7];
 lista.map(function (x) { return x * x; })
     .forEach(function (x) { return console.log(x); });
+// Generic challenge
+var Paleta = /** @class */ (function () {
+    function Paleta(mode) {
+        if (mode === void 0) { mode = "RGB"; }
+        if (mode === "RGB") {
+            this.colors = [];
+        }
+    }
+    Paleta.prototype.randomRGBColor = function () {
+        return Math.floor(Math.random() * 256);
+    };
+    Paleta.prototype.addRandomRGBtoPallet = function () {
+        this.colors.push({
+            red: this.randomRGBColor(),
+            green: this.randomRGBColor(),
+            blue: this.randomRGBColor()
+        });
+    };
+    return Paleta;
+}());
+var paleta = new Paleta();
+paleta.addRandomRGBtoPallet();
+console.log(paleta.colors);
