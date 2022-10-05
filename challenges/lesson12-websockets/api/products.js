82991ef4f18ef66e7d83f01fb50a49e6c60b49ba
Heltta <lucashelgueraz@gmail.com>
2022-10-05 11:44:39 -0300

    Copy paste files from last challenge

---
+++ challenges/lesson12-websockets/api/products.js
@@ -0,0 +1,67 @@
+const Contenedor = require('../modules/contenedor.js');
+const express = require('express')
+const { Router } = express;
+
+const router = Router();
+
+const products = new Contenedor('./uploads/productos.json');
+
+router.get('/', (req, res) => {
+    products.getAll()
+        .then( (products) => res.render(
+            'viewProducts',
+            {items: products, listExists: _ => products.length !== 0}))
+})
+
+router.post('/', (req, res) => {
+    const prod = {
+        title: req.body.title,
+        price: req.body.price,
+        thumbnail: req.body.thumbnail
+    }
+    res.status(102);
+    products.save(prod)
+        .then((id)=> res.status(201).redirect('/'))
+        .catch( error => console.log(error));
+})
+
+// Eventos restful
+router.get('/api/:id', (req, res) => {
+    products.getById(parseInt(req.params.id))
+        .then((product)=>(product.length === 0)?
+            res.status(404).send({ error : 'producto no encontrado' }) :
+            res.status(302).send(product));
+})
+
+router.post('/api/', (req, res) => {
+    const prod = {
+        title: req.body.title,
+        price: req.body.price,
+        thumbnail: req.body.thumbnail
+    }
+    res.status(102);
+    products.save(prod)
+        .then((id)=> res.status(201).send({ id }))
+        .catch( error => console.log(error));
+})
+
+router.put('/api/:id', (req, res) => {
+    const prod = {
+        title: req.body.title,
+        price: req.body.price,
+        thumbnail: req.body.thumbnail
+    }
+    res.status(102);
+    products.overwriteById(parseInt(req.params.id), prod)
+        .then( () =>res.status(201).send())
+        .catch( error => console.log(error));
+})
+
+router.delete('/api/:id', (req, res) => {
+    res.status(102);
+    products.deleteById(parseInt(req.params.id))
+        .then( () =>res.status(200).send())
+        .catch( error => console.log(error));
+})
+
+module.exports = router;
---
+++ challenges/lesson12-websockets/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const products = require('./api/products.js');
+
+const app = express();
+
+//load bootstrap
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+
+//same as app.use(express.json())
+const { json } = express;
+app.use(json());
+
+//load products api
+const dirProducts = '/api/productos';
+app.use(dirProducts, products);
+
+app.set('view engine', 'pug'); // register template engine
+app.set('views', './views'); // set template files folder
+app.use(express.static("./public")) // set server public space
+
+app.use(express.urlencoded({ extended: true}));
+
+app.use('/productos', products);
+app.use(express.static("./public"))
+
+
+// renderizo una vista de home
+app.get('/',  (req, res) => {
+    res.render('./sendProduct.pug', {mensaje: 'Usando Pug JS en express'});
+});
+
+// Start server
+const PORT = process.env.PORT || 8080
+const server = app.listen(PORT, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+});
+server.on("error", error => console.log(`Error en servidor ${error}`));
+
