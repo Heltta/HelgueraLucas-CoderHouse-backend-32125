d538f66001e66a5c3d87a3b3d39299837083b10c
Heltta <lucashelgueraz@gmail.com>
2022-10-06 23:36:52 -0300

    Render products send through websocket
    Render all products send through websocket under form for adding products.

--- challenges/lesson12-websockets/public/websocket/itemList.js
+++ challenges/lesson12-websockets/public/websocket/itemList.js
@@ -7,6 +7,40 @@ socket.on('req-api-type', _ => {
     socket.emit('res-api-type', 'itemList');
 })
 
+const parseIntoRow = (id, name, price, thumbnail) =>{
+    return `
+    <tr>
+        <th scope="row">${id}</th>
+        <td>${name}</td>
+        <td>${price}</td>
+        <td>
+            <img src="${thumbnail}" 
+                alt="${name}" 
+                width="64" 
+                height="64">
+        </td>
+    </tr>
+    `
+}
+
 socket.on('welcome', data => {
     console.log(data);
+
+    if(data.length !== 0){
+        console.log(data.length);
+        //Write all items if not empty
+        const tableBody = document.querySelector("#productTable tbody");
+        tableBody.innerHTML = '';
+        data.forEach( product => {
+            tableBody.innerHTML = tableBody.innerHTML.concat(
+                parseIntoRow(
+                    product.id,
+                    product.title,
+                    product.price,
+                    product.thumbnail,)
+            )
+        });
+    };
+    
+    socket.emit('tabla recibida', 'Gracias por recibirme')
 })
