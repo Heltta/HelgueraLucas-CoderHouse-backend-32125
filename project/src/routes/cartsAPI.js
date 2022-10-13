5a35c68a3ac05273547683b389f95b594944bdc6
Heltta <lucashelgueraz@gmail.com>
2022-10-13 10:52:03 -0300

    Implement create cart

--- project/src/routes/cartsAPI.js
+++ project/src/routes/cartsAPI.js
@@ -9,4 +9,17 @@ const carts = new Container('data/carts.json');
 
 /////// HTTP request methods routing //////
 
+router.post('/', (req, res) =>{
+    // Create (and save) a cart and returns its ID
+    if(!(req.body)){
+        res.status(400).send('Error: body is empty');
+        return
+    }
+    const cart = new Cart(req.body);
+    res.status(102);
+    carts.save(cart)
+        .then((id)=> res.status(201).send({ id }))
+        .catch( error => console.log(error));
+})
+
 export default router;
