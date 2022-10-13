c5b114a2f8cd60d5d215564783552300ed28f970
Heltta <lucashelgueraz@gmail.com>
2022-10-13 10:22:09 -0300

    Set up cartsAPI.js for development
    Add imports needed and create Container needed for the API.
    
    Fill "carts.json" with placeholder content.

--- project/src/routes/cartsAPI.js
+++ project/src/routes/cartsAPI.js
@@ -1,7 +1,11 @@
+import Container from '../controllers/container.js';
+import Cart from '../models/cart.js';
 import express from 'express';
 const { Router } = express;
 
 const router = Router();
+
+const carts = new Container('data/carts.json');
 
 /////// HTTP request methods routing //////
 
