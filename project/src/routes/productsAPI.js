f37523f38804fffd37184470b6a49095c8d65d1a
Heltta <lucashelgueraz@gmail.com>
2022-10-13 01:11:34 -0300

    Minimum viable ProductsAPI

--- project/src/routes/productsAPI.js
+++ project/src/routes/productsAPI.js
@@ -1,50 +1,53 @@
 import Container from '../controllers/container.js';
+import Product from '../models/product.js';
 import express from 'express';
 const { Router } = express;
 
 const router = Router();
 
-const products = new Container('../data/products.json');
+const products = new Container('data/products.json');
 
 /////// HTTP request methods routing //////
 
-router.get('/api/', (req, res) => {
+router.get('/', (req, res) => {
     products.getAll()
-        .then((products)=>res.send(products));
+        .then((products)=>(products)?
+            res.status(302).send(products) : 
+            res.status(404).send('Error: no products found'));
 })
 
-router.get('/api/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     products.getById(parseInt(req.params.id))
         .then((product)=>(product.length === 0)?
-            res.status(404).send({ error : 'producto no encontrado' }) :
+            res.status(404).send('Error: no matches found') :
             res.status(302).send(product));
 })
 
-router.post('/api/', (req, res) => {
-    const prod = {
-        title: req.body.title,
-        price: req.body.price,
-        thumbnail: req.body.thumbnail
+router.post('/', (req, res) => {
+    if(!(req.body)){
+        res.status(400).send('Error: body is empty');
+        return
     }
+    const prod = new Product(req.body);
     res.status(102);
     products.save(prod)
         .then((id)=> res.status(201).send({ id }))
         .catch( error => console.log(error));
 })
 
-router.put('/api/:id', (req, res) => {
-    const prod = {
-        title: req.body.title,
-        price: req.body.price,
-        thumbnail: req.body.thumbnail
+router.put('/:id', (req, res) => {
+    if(!(req.body)){
+        res.status(400).send('Error: body is empty');
+        return
     }
+    const prod = new Product(req.body);
     res.status(102);
     products.overwriteById(parseInt(req.params.id), prod)
         .then( _ =>res.status(201).send())
         .catch( error => console.log(error));
 })
 
-router.delete('/api/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     res.status(102);
     products.deleteById(parseInt(req.params.id))
         .then( () =>res.status(200).send())
