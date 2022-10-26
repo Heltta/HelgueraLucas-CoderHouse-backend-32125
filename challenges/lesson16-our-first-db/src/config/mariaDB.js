6cd910a700b850f78e4cf060efef8cee5e0d3a1d
Heltta <lucashelgueraz@gmail.com>
2022-10-26 13:37:03 -0300

    Add port to Knex MariaDB options

--- challenges/lesson16-our-first-db/src/config/mariaDB.js
+++ challenges/lesson16-our-first-db/src/config/mariaDB.js
@@ -6,8 +6,9 @@ const options = {
         host: '127.0.0.1',
         user: 'root',
         password: '',
-        database: 'test'
-    }
+        database: 'test',
+        port : 3306,
+    },
 };
 
 const myknex = knex(options)
