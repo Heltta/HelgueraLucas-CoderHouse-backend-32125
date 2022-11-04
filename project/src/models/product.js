b6e06e5c010911c9b3f97b8a44b7e83a7d3932c2
Heltta <lucashelgueraz@gmail.com>
2022-11-03 21:58:42 -0300

    Update product.js

--- project/src/models/product.js
+++ project/src/models/product.js
@@ -1,3 +1,4 @@
+import myknex from '../config/mariaDB.js';
 
 class Product {
     constructor({id, name, description, code, photoURL, price, stock}){
@@ -10,6 +11,42 @@ class Product {
         this.price = price  || "";
         this.stock = stock  || "";
     }
+    
+    static tableFields = [
+        {key:'id', type: 'increments'},
+        {key: 'timestamp', type: 'unsigned biginteger'},
+        {key: 'name', type: 'string'},
+        {key: 'description', type: 'text'},
+        {key: 'code', type: 'string'},
+        {key: 'photo', type:'string'},
+        {key: 'price', type: 'integer'},
+        {key: 'stock', type: 'integer'}
+    ]
+
+    static #dataBase = myknex;
+
+    static createTable(tableName){
+        this.#dataBase.schema.hasTable(tableName).then( exists =>{
+            if(exists){
+                console.log("table already exists");
+            }
+            else{
+                this.#dataBase.schema.createTable(tableName, table => {
+                    table.increments('id');
+                    table.bigint('timestamp').unsigned();
+                    table.string('name');
+                    table.text('description');
+                    table.string('code');
+                    table.string('photo');
+                    table.integer('price');
+                    table.integer('stock');
+                })
+                    .then(  _ => console.log("table created"))
+                    .catch( (err) => {console.log(err); throw err});
+
+            }
+        })
+    }  
 }
 
 // export default Container;
