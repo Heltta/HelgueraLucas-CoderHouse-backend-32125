2f78cf10919ad6b27c1c79531ae0c8f621225255
Heltta <lucashelgueraz@gmail.com>
2022-12-11 19:58:26 -0300

    Create blocking http request
    Create an http get request handler which calls synchronously a function with a high processing time (intended).

--- generic-challenges/lesson-28-g02-child_process-with-fork/src/routes/main.routes.js
+++ generic-challenges/lesson-28-g02-child_process-with-fork/src/routes/main.routes.js
@@ -2,12 +2,19 @@ import {
     Router
 } from 'express';
 
+import summation  from '../childs/summation.js';
+
 const baseRoute = Router();
 
 let visitCounter = 0;
 baseRoute.get('/', (req, res) =>{
     visitCounter++;
     res.send(`This endopoint has been visited ${visitCounter} times`)
+})
+
+baseRoute.get('/calculation-blocking', (req, res)=>{
+    summation();
+    res.send('Bloquing sumatory finished');
 })
 
 export default baseRoute;
