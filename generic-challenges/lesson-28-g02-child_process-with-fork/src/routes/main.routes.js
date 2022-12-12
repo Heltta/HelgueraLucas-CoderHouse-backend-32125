62d5f55d0dbd0be2d16d678f2812e77e63854ff6
Heltta <lucashelgueraz@gmail.com>
2022-12-11 21:34:34 -0300

    Show execution flow
    Add multiple "console.log()" calls to see how the server handles the different requests. Each "console.log()" signalize the when a new request is made or is fulfilled.

--- generic-challenges/lesson-28-g02-child_process-with-fork/src/routes/main.routes.js
+++ generic-challenges/lesson-28-g02-child_process-with-fork/src/routes/main.routes.js
@@ -18,21 +18,27 @@ const baseRoute = Router();
 
 let visitCounter = 0;
 baseRoute.get('/', (req, res) =>{
+    console.log('A visit counter request has been made');
     visitCounter++;
     res.send(`This endopoint has been visited ${visitCounter} times`)
+    console.log('Visit counter request finished');
 })
 
 baseRoute.get('/calculation-blocking', (req, res)=>{
+    console.log('A blocking calculation request has been made');
     summation();
     res.send('Bloquing sumatory finished');
+    console.log('Blocking calculation counter request finished');
 })
 
 baseRoute.get('/calculation-noblocking', (req, res)=>{
+    console.log('A non blocking calculation request has been made');
     const forked = fork(`${__dirname}/../childs/nonblocking.summation.js`);
     forked.on('message', msg => {
         if(msg === 'ready') forked.send('start');
         else {
             res.send('Non bloquing sumatory finished');
+            console.log('Non blocking calculation counter request finished');
         };
     })
 })
