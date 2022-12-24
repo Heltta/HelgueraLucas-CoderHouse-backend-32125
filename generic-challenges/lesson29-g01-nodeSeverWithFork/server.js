7b73960acf21058870e59c63e6247916d27a019c
Heltta <lucashelgueraz@gmail.com>
2022-12-24 17:04:11 -0300

    Create clusters
    Set up primary process and fork worker process.
    
    Each worker can answer an HTTP get petition at root. Response to this petition is a message which carries the server port, the worker (that handled the request) process ID, and the current date).

--- generic-challenges/lesson29-g01-nodeSeverWithFork/server.js
+++ generic-challenges/lesson29-g01-nodeSeverWithFork/server.js
@@ -13,11 +13,36 @@ const httpServer = new HttpServer(app);
 //-- Express middleware ---------//
 app.use(json());
 app.use(urlencoded({ extended:true }));
+//-- Cluster --//
+import cluster from 'cluster';
+import os from 'os';
+const numCPUs = os.cpus().length;
 
-//////////// Turn on server ///////////
 import { server_port } from './config/CLIarguments.js';
-const PORT = server_port || 8080
-const server = httpServer.listen(PORT, () => {
-    console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
-});
-server.on('error', error => console.log(`Error en servidor ${error}`));
+
+if(cluster.isPrimary){
+    console.log(`Master ${process.pid} is running`);
+
+    //Fork workers
+    for(let i=0; i<numCPUs; i++) cluster.fork();
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`worker ${worker.process.pid} died`)
+    })
+} else {
+    // Worker can share any TCP connection
+    // In this case it is an HTTP server
+
+    const PORT = server_port || 8080
+    app.get('/', (req, res) =>{
+        res.send(`Express server on Port ${PORT} - PID ${process.pid} - ${new Date()}`) 
+    })
+
+    //////////// Turn on server ///////////
+    const server = httpServer.listen(PORT, () => {
+        console.log(`Servidor http escuchando en el puerto ${server.address().port}`)
+    });
+    server.on('error', error => console.log(`Error en servidor ${error}`));
+
+    console.log(`Worker ${process.pid} started`);
+}
