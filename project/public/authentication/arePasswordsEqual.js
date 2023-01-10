4495034c4be6de9504c7e2ba2feed967d7b933fc
Heltta <lucashelgueraz@gmail.com>
2023-01-10 02:23:43 -0300

    Update arePasswordsEqual.js
    Create new function named "areFieldsMatched". The new function checks equality of two input fields.
    
    Require that both email input values are equal before post request. Add an alert for when form is submit without this requirement.

--- project/public/authentication/arePasswordsEqual.js
+++ project/public/authentication/arePasswordsEqual.js
@@ -1,12 +1,33 @@
 
-const passwordOriginal = document.querySelector("#inputPassword");
-const passwordConfirm = document.querySelector("#confirmPassword");
+
+/**
+ * Compares two inputs values for equality.
+*
+* Searches two input fields (both must exist) of a given id, then compares their values for equality.
+*
+* @see document.getElementById()
+* 
+* @param   {String}    fieldA      Id of the first input tag.
+* @param   {String}    fieldB      Id of the second input tag.
+* 
+* @return  {Boolean}   True if value of both fields are equal, false otherwise.
+*/
+function areFieldsMatched(fieldA,fieldB){
+    const valueA = document.getElementById(fieldA).value;
+    const valueB = document.getElementById(fieldB).value;
+    
+    return (valueA === valueB);
+}
 
 const signup = document.querySelector("#session_signup");
 
 signup.addEventListener('submit', e =>{
-    if(passwordOriginal.value !== passwordConfirm.value){
+    if(!areFieldsMatched("inputPassword","confirmPassword")){
         e.preventDefault();
         alert('Password confirmation error: passwords are not equal');
+    }
+    else if(!areFieldsMatched("inputEmail","confirmEmail")){
+        e.preventDefault();
+        alert('Email confirmation error: emails are not equal');
     }
 });
