f7247925beec2255f5c761d0425f33fff1d2211b
Heltta <lucashelgueraz@gmail.com>
2023-02-03 04:28:26 -0300

    Fix minimum test script while using supertest

--- project/__tests__/endpoints.test.js
+++ project/__tests__/endpoints.test.js
@@ -3,41 +3,27 @@ import {
     expect,
     test,
     afterAll,
-    beforeAll
 } from '@jest/globals';
 import { faker } from "@faker-js/faker";
 import supertest from 'supertest';
 
 
 //////////// Server config ////////////
-import {
-    SERVER_INTERFACE,
-    SERVER_PORT as auxiliarServerPort,
-} from '../src/config/dotEnVar.js';
-import { 
-    server_port as primaryServerPort
-} from '../src//config/CLIarguments.js';
 import { 
     app,
 } from '../src/app.js';
 import ContainerMongoDB from '../src/controllers/containerMongoDB.js';
 
-let server;
-beforeAll((done) => {
-    const PORT = primaryServerPort || auxiliarServerPort;
-    server = app.listen(PORT, SERVER_INTERFACE, (err) => {
-      if (err) return done(err);
-       done();
-    });
-});
 
-afterAll((done) => {
+afterAll(() => {
     ContainerMongoDB.disconnectDB();
-    return server && server.close(done);
 });
 
-test('should pass the test', () => {
-    expect(2).toBe(2);
+test('should pass the test', async () => {
+    const response = await supertest(app).get("/");
+    expect(
+        response
+    ).toHaveProperty("statusCode", 200);
 });
 
 describe.skip("/api/products", () =>{
