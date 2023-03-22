329e0c3ae4b5d8fb128880ebef1fdb5100352124
Heltta <lucashelgueraz@gmail.com>
2023-03-22 00:05:31 -0300

    Attach user to chat message

--- project/src/lib/socketChatEvent.js
+++ project/src/lib/socketChatEvent.js
@@ -1,7 +1,7 @@
 import logger from '../config/logger.js';
 import MessagesDao from '../daos/messagesDao.js';
 import Message from '../models/message.js';
-import UsersGeneralDao from '../daos/usersDao.js';
+import UsersDao from '../daos/usersDao.js';
 import User from '../models/user.js';
 
 function attachChatEventToSocket(socket) {
@@ -9,25 +9,27 @@ function attachChatEventToSocket(socket) {
 
     socket.on('chat', async (callback) => {
         logger.info(`"chat" socket event triggered at socket ${socket.id}`);
+        const sessionUser = socket.request.session.passport.user;
+        const usersDao = UsersDao.getInstance();
         const messagesDao = MessagesDao.getInstance();
+        const user = new User(
+            await usersDao.findOne({ email: sessionUser.email })
+        );
+
         try {
-            // const msgHistory = await messagesDao.getAll();
-            const usersDao = UsersGeneralDao.getInstance();
-            const allUsers = await usersDao.getAll();
-            const msgHistory = [
-                new Message({ user: allUsers[0], content: 'asdf asdf asdf' }),
-                new Message({ user: allUsers[1], content: 'agagagaga alala' }),
-            ];
+            const msgHistory = await messagesDao.getAll();
             callback(msgHistory);
             logger.info(`chat history sent`);
         } catch (error) {
             logger.error(error);
         }
+
         socket.on('new_user_message', async (data) => {
             logger.info(
                 `"new_user_message" socket event triggered at socket ${socket.id}`
             );
-            const msg = new Message(data);
+            logger.info(`"new_user_message" content is ${data}`);
+            const msg = new Message({ content: data, user: user });
             try {
                 await messagesDao.save(msg);
                 logger.info(`new message sent`);
