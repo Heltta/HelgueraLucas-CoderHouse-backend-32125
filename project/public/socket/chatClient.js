395ccded636dcaa004b016aceec69bec6c7a4354
Heltta <lucashelgueraz@gmail.com>
2023-03-23 19:20:11 -0300

    Refactor chatClient
    Refactor code to respect Single Responsibility Principle.
    Transform ejsTemplate into a globar variable.
    Update functions to make use of their closures to access ejsTemplate.

--- project/public/socket/chatClient.js
+++ project/public/socket/chatClient.js
@@ -3,32 +3,25 @@
 const socket = io();
 const { render } = ejs;
 
-socket.emit('chat', (response) => {
+let ejsTemplate;
+
+socket.emit('chat', async (response) => {
     // Response contains chat message history
-    renderMessages(response);
-    document.getElementById('message_box').addEventListener('submit', (e) => {
-        e.preventDefault();
-        socket.emit(
-            'new_user_message',
-            document.getElementById('message').value
-        );
-    });
-});
 
-async function renderMessages(response) {
     // fetch ejs template for message
-    const ejsTemplate = await fetchChatBoxTemplate();
+    ejsTemplate = await fetchChatBoxTemplate();
 
-    // render fetched template
-    applyRenderedTemplate({
-        template: ejsTemplate,
-        data: response,
-        elementID: 'chat_box',
-    });
+    // Render chat history inside chat_box
+    renderMessages(response);
 
+    // Attach event for new user message to socket
     socket.on('new_user_message', (data) => {
         // add message to stream
         response.push(data);
+
+        // Update chat content inside chat_box
+        renderMessages(response);
+    });
 
     // Change form submit event to emit new message
     document.getElementById('message_box').addEventListener('submit', (e) => {
@@ -40,27 +33,22 @@ async function renderMessages(response) {
         } else {
             console.warn('Tried to send empty message');
         }
-        });
     });
+});
+
+function renderMessages(messages) {
+    // render fetched template
+    let renderedView = render(ejsTemplate, { messages: messages });
+    const elementDOM = document.getElementById('chat_box');
+    elementDOM.innerHTML = renderedView;
 }
 
 async function fetchChatBoxTemplate() {
     try {
-        const fetchedTemplate = await fetch('views/templates/chatBox.ejs');
-        const ejsTemplate = await fetchedTemplate.text();
-        return ejsTemplate;
+        const response = await fetch('views/templates/chatBox.ejs');
+        const receivedTemplate = await response.text();
+        return receivedTemplate;
     } catch (error) {
         console.log(error);
     }
-}
-
-function applyRenderedTemplate({
-    template,
-    data,
-    elementID = 'chat_box',
-} = {}) {
-    // render fetched template
-    let renderedView = render(template, { messages: data });
-    const elementDOM = document.getElementById(elementID);
-    elementDOM.innerHTML = renderedView;
 }
